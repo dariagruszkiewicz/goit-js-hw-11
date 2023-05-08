5f31564f9af1fd8d318533647851c73942f3a1b8
Daria Gruszkiewicz <dariaa.pajak@gmail.com>
2023-05-08 23:09:47 +0200

    update 3
    update 3

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 const axios = require('axios').default;
 const KEY = '35804077-868b4923c4d4b0980765f3041';
 const inputEl = document.querySelector('input[type="text"]');
 const formEl = document.getElementById('search-form');
 const containerEl = document.querySelector('.gallery');
-console.log(containerEl);
+const loadMoreBtn = document.querySelector('.load-more');
+let page = 1;
+let perPage = 40;
+loadMoreBtn.style.display = 'none';
 
-formEl.addEventListener('submit', e => {
-  e.preventDefault();
-  const names = inputEl.value;
-  console.log(names);
-});
-
-const getData = async name => {
+const createGallery = async name => {
   const API_URL = 'https://pixabay.com/api/?';
 
   await axios
@@ -23,22 +22,28 @@ const getData = async name => {
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
+        page,
+        per_page: perPage,
       },
     })
     .then(res => {
+      loadMoreBtn.style.display = 'flex';
       const item = res;
       const itemData = item.data.hits;
-      console.log(itemData);
 
       containerEl.textContent = '';
       itemData.map(item => {
         const photoCard = document.createElement('div');
         photoCard.classList.add('photo-card');
         containerEl.append(photoCard);
+        const imageRef = document.createElement('a');
+        imageRef.href = item.largeImageURL;
+        photoCard.append(imageRef);
         const imageEl = document.createElement('img');
         imageEl.src = item.webformatURL;
-        imageEl.alt = item => item.tags;
+        imageEl.alt = item.tags;
         imageEl.setAttribute('loading', 'lazy');
+        imageRef.append(imageEl);
         const infoImage = document.createElement('div');
         infoImage.classList.add('info');
         infoImage.innerHTML = `
@@ -58,23 +63,44 @@ const getData = async name => {
         <b>Downloads</b><br>
         ${item.downloads}
       </p>`;
-        photoCard.append(imageEl, infoImage);
+        photoCard.append(infoImage);
       });
 
+      let gallery = new SimpleLightbox('.gallery a');
+      gallery.on('show.simplelightbox');
+
       if (itemData.length === 0) {
+        loadMoreBtn.style.display = 'none';
         Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
       } else {
-        const totalHits = itemData.length;
+        const totalHits = item.data.total;
         Notify.success(`Hooray! We found ${totalHits} images.`);
+
+        if (page > totalHits / perPage) {
+          loadMoreBtn.style.display = 'none';
+          Notify.info(
+            "We're sorry, but you've reached the end of search results."
+          );
+        }
       }
     })
+
     .catch(err => console.log(err));
+};
+
+const showNextPage = () => {
+  page++;
+  const names = inputEl.value;
+  createGallery(names);
 };
 
 formEl.addEventListener('submit', e => {
   e.preventDefault();
+  page = 1;
   const names = inputEl.value;
-  getData(names);
+  createGallery(names);
 });
+
+loadMoreBtn.addEventListener('click', showNextPage);
